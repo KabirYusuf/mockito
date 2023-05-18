1a12872e3bd06ce478637efc42e94c208a736ba5
KabirYusuf <sonkaybee@gmail.com>
2023-05-18 22:53:40 +0100

    Global exception handler added

---
+++ src/main/java/com/kyaa/mockito/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.kyaa.mockito.exception;
+
+public class GlobalExceptionHandler {
+}
--- src/main/java/com/kyaa/mockito/exception/UserException.java
+++ src/main/java/com/kyaa/mockito/exception/UserException.java
@@ -1,4 +1,7 @@
 package com.kyaa.mockito.exception;
 
 public class UserException extends RuntimeException{
+    public UserException(String message){
+        super(message);
+    }
 }
