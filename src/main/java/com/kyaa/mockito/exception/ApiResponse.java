09f8e42644f393d664b14e4cbdf75eea2b876aa0
KabirYusuf <sonkaybee@gmail.com>
2023-05-18 22:52:17 +0100

    User exception added

--- src/main/java/com/kyaa/mockito/exception/ApiResponse.java
+++ src/main/java/com/kyaa/mockito/exception/ApiResponse.java
@@ -1,4 +1,14 @@
 package com.kyaa.mockito.exception;
 
+import lombok.Data;
+
+import java.time.ZonedDateTime;
+
+@Data
 public class ApiResponse {
+    private String path;
+    private ZonedDateTime zonedDateTime;
+    private Object response;
+    private boolean isSuccessful;
+    private int statusCode;
 }
